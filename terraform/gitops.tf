23f74a37147d9e187d2a5881f677271571226dff
Mahmood LotfaliZade <mahmood.lotfalizade@gmail.com>
2026-05-26 12:54:37 +0200

    add dispatcher cloudwatch logs to filebeat inputs

--- terraform/gitops.tf
+++ terraform/gitops.tf
@@ -160,6 +160,11 @@ values = [
                         region: "${var.aws_region}"
                         scan_frequency: 1m 
                         api_timeout: 120s       
+                      - type: aws-cloudwatch
+                        log_group_arn: "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/linkedin-dispatcher-function:*"
+                        region: "${var.aws_region}"
+                        scan_frequency: 1m
+                        api_timeout: 120s
                     output.logstash:
                       hosts: ["logstash-logstash:30092"]
           - repoURL: 'https://github.com/Mahmood-Lm/JobsBot.git'
